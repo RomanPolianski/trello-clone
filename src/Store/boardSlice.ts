814c843ac33da0d9eacc09f7e92415674c5738d2
Roman <rpol123453@gmail.com>
2022-04-24 22:13:00 +0300

    Add addTask & addList reducers

--- src/Store/boardSlice.ts
+++ src/Store/boardSlice.ts
@@ -1,5 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+let boardId = 3;
+let taskId = 6;
+
 const boardSlice = createSlice({
   name: 'board',
   initialState: {
@@ -38,9 +41,30 @@ const boardSlice = createSlice({
       },
     ],
   },
-  reducers: {},
+  reducers: {
+    addList(state, action) {
+      const newList = {
+        boardId: boardId,
+        taskList: [],
+        boardName: action.payload,
+      };
+      boardId += 1;
+      state.boards.push(newList);
+    },
+    addTask(state, action) {
+      const newTask = {
+        id: taskId,
+        text: action.payload.inputText,
+      };
+      taskId += 1;
+
+      const list = state.boards
+        .find((board) => board.boardId === action.payload.boardId)
+        ?.taskList.push(newTask);
+    },
+  },
 });
 
-export const {} = boardSlice.actions;
+export const { addList, addTask } = boardSlice.actions;
 
 export default boardSlice.reducer;

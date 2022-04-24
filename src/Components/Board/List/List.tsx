f61855645126cf6c8af8f0bcf6d98b97952708dc
Roman <rpol123453@gmail.com>
2022-04-24 17:36:07 +0300

    Create List component

---
+++ src/Components/Board/List/List.tsx
@@ -0,0 +1,21 @@
+import { ListPropsType } from '../../../types/types';
+import { FC } from 'react';
+import Task from '../Task/Task';
+import s from './List.module.css';
+import ActionButton from '../../common/ActionButton/ActionButton';
+
+const List: FC<ListPropsType> = ({ title, taskList }) => {
+  return (
+    <div className={s.listContainer}>
+      <h2>{title}</h2>
+      {taskList.map((task) => (
+        <Task text={task.text} key={task.id} />
+      ))}
+      <div>
+        <ActionButton card="card" list="" />
+      </div>
+    </div>
+  );
+};
+
+export default List;

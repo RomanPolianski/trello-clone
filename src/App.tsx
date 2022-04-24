d558411f5f370e6aab844a31295aceada50271bb
Roman <rpol123453@gmail.com>
2022-04-24 17:43:09 +0300

    Add List to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
 import './App.css';
+import List from './Components/Board/List/List';
+import ActionButton from './Components/common/ActionButton/ActionButton';
+import { RootState } from './Store/redux-store';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
+  const lists = useSelector((state: RootState) => state.board.boards);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.tsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="listContainer">
+      {lists.map((list) => (
+        <List
+          key={list.boardId}
+          title={list.boardName}
+          taskList={list.taskList}
+        />
+      ))}
+      <div>
+        <ActionButton list="list" card="" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;

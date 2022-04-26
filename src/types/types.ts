038797d98a76675c9e5d6aeef564a74cbe97b6e3
Roman <rpol123453@gmail.com>
2022-04-26 20:09:52 +0300

    Add types for initial state

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,37 @@
 export interface ListPropsType {
-  boardId: number;
+  boardId: string;
   title: string;
   taskList: Array<TaskListType>;
 }
 
 export interface TaskListType {
-  id: number;
+  id: string;
   text: string;
 }
 
 export interface TaskPropsType {
   text: string;
+  id: string;
+  index: number;
 }
 
 export interface ActionButtonPropsType {
   list: string;
   card: string;
-  boardId: number | null;
+  boardId: string | null;
+}
+
+export interface BoardStateType {
+  boards: Array<BoardType>;
+}
+
+export interface BoardType {
+  boardId: string;
+  boardName: string;
+  taskList: Array<TaskListType>;
+}
+
+export interface TaskListType {
+  id: string;
+  text: string;
 }

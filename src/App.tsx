69292661ce047713d194ef6dc1af0cd89c84720f
Roman <rpol123453@gmail.com>
2022-04-26 20:09:05 +0300

    Add onDragEnd function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import List from './Components/Board/List/List';
 import ActionButton from './Components/common/ActionButton/ActionButton';
 import { RootState } from './Store/redux-store';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { dragHappened } from './Store/boardSlice';
 
 const App = () => {
   const lists = useSelector((state: RootState) => state.board.boards);
+  const dispatch = useDispatch();
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    dispatch(
+      dragHappened({
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+      })
+    );
+  };
+
   return (
-    <div className="listContainer">
-      {lists.map((list) => (
-        <List
-          boardId={list.boardId}
-          key={list.boardId}
-          title={list.boardName}
-          taskList={list.taskList}
-        />
-      ))}
-      <div>
-        <ActionButton list="list" card="" boardId={null} />
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="listContainer">
+        {lists.map((list) => (
+          <List
+            boardId={list.boardId}
+            key={list.boardId}
+            title={list.boardName}
+            taskList={list.taskList}
+          />
+        ))}
+        <div>
+          <ActionButton list="list" card="" boardId={null} />
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 };
 

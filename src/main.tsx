9d063fc14e55565a78273a030adfbc5fb436bf57
Roman <rpol123453@gmail.com>
2022-04-27 18:21:01 +0300

    Fix dragging on hardcoded tasks

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,7 @@ import './index.css';
 import store from './Store/redux-store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-     <Provider store={store}>
-        <App />
-      </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
